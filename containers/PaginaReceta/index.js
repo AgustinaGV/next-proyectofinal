f9b626706355306ecd375175046c7043a2d9ec3c
AgustinaGV <agustinagarciavega@gmail.com>
2020-11-26 04:42:58 -0300

    Avance estilos page receta

--- containers/PaginaReceta/index.js
+++ containers/PaginaReceta/index.js
@@ -1,8 +1,11 @@
 import {Container as PaginaRecetaContainer} from './styled'
 
-import Support from '../../images/support.svg'
-import CrueltyFree from '../../images/cruelty-free.svg'
+
+import RecipeBook from '../../images/recipe-book.svg'
+import Plate from '../../images/plate.svg'
+import CookingTime from '../../images/preparation.svg'
 import Prepare from '../../images/prepare.svg'
+import Carrots from '../../images/carrots.svg'
 
 const PaginaReceta = ({props}) => {
 
@@ -19,17 +22,62 @@ const PaginaReceta = ({props}) => {
     
     return (
         <PaginaRecetaContainer>
-            <h1>{props.titulo}</h1>
             <div>
-                <div>Receta: {props.titulo}</div>
-                <div>Descripción: {props.descripcion}</div>
-                <div>Categoria: {props.categoria}</div>
-                <div>Ingredientes: {props.ingredientes}</div>
-                <div>Tiempo de preparación: {props.tiempoPreparacion}</div>
-                <div>Tiempo de cocción: {props.tiempoCoccion}</div>
-                <div>Rinde: {props.porciones} porciones</div>
-                <div>Paso a paso: {props.pasos}</div>
+                <div className="tituloReceta">
+                    <h1>{props.titulo}</h1>
+                    <img src={RecipeBook} alt={RecipeBook}/>
+                </div>
+                <div>
+                    <span className="subtitulos">Categoría </span>
+                    <p>{props.categoria}</p>
+                </div>
             </div>
+
+            <div>
+                <span className="subtitulos">Descripción </span>
+                <p>{props.descripcion}</p>
+            </div>
+
+            <div>
+                <div>
+                    <div className="subtituloConIcono">
+                        <span className="subtitulos">Tiempo de preparación </span>
+                        <img src={CookingTime} alt={CookingTime} className="iconos"/>
+                    </div>
+                    <p>{props.tiempoPreparacion}</p>
+                </div>
+                <div>
+                    <div className="subtituloConIcono">
+                        <span className="subtitulos">Tiempo de cocción </span>
+                        <img src={CookingTime} alt={CookingTime} className="iconos"/>
+                    </div>
+                    <p>{props.tiempoCoccion}</p>
+                </div>
+                <div>
+                    <div className="subtituloConIcono">
+                        <span className="subtitulos">Rinde </span>
+                        <img src={Plate} alt={Plate} className="iconos"/>
+                    </div>
+                    <p>{props.porciones}</p>
+                </div>
+            </div>
+
+            <div>
+                <div className="subtituloConIcono">
+                    <span className="subtitulos">Ingredientes </span>
+                    <img src={Carrots} alt={Carrots} className="iconos"/> 
+                </div>
+                <p>{props.ingredientes}</p>
+            </div>
+            <div>
+                <div className="subtituloConIcono">
+                    <span className="subtitulos">Paso a paso</span>
+                    <img src={Prepare} alt={Prepare} className="iconos"/>
+                </div>
+                <p>{props.pasos}</p>
+            </div>
+
+            <div className="botonForm">Agregar a mis recetas favoritas</div>
         </PaginaRecetaContainer>
     )
 }
--- containers/PaginaReceta/styled.js
+++ containers/PaginaReceta/styled.js
@@ -2,8 +2,78 @@ import styled from 'styled-components'
 
 const Container = styled.div `
 
+color:black;
+
     h1 {
         color:black;
+    }
+
+    
+
+    div {
+        margin-bottom:1.5em;
+    }
+
+    .tituloReceta {
+
+        display:flex;
+        justify-content:left;
+        align-items:center;
+
+        img {
+
+            height:2em;
+            margin-left:1em;
+        }
+        
+    }
+
+    .subtituloConIcono {
+
+        display:flex;
+        justify-content:left;
+        align-items:center;
+
+        img {
+
+            margin-left:0.7em;
+        }
+
+    }
+    
+
+    .subtitulos {
+
+        color:black;
+        display:block;
+        font-size: 1.3em;
+        font-weight:700;
+    }
+
+    .iconos {
+
+        height:1.5em;
+    }
+
+    .botonForm {
+
+        background-color:MediumAquaMarine;
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto, Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue, sans-serif;
+        text-align:center;
+        font-weight:500;
+        font-size: 1.2em;
+        width:20em;
+        border-radius:0;
+        color:white;
+        padding: 0.7em 1em 0.7em 1em;
+        margin: 0 auto;
+        margin-top:3em;
+    }
+
+    .botonForm:hover{
+        //background-color:MediumSeaGreen;
+        cursor: no-drop;
+        background-color:grey;
     }
 
 `
